89f05ade64d71f70056e7083560ae674d562c04d
cabinboy1031 <hemmerian@gmail.com>
2022-07-14 07:34:23 -0700

    Changed usage message to avoid confusion

--- chat.c
+++ chat.c
@@ -82,7 +82,7 @@ int main(int argc, char* argv[]){
   */
 
   if(argc != 2){
-    printf("Usage: ./chat [client | server]\n");
+    printf("Usage: ./chat [c | s]\n");
     return -1;
   }
   ARGV_RUN("c", client_run());
